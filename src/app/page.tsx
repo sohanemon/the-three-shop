1e18aa8ce9ea83e19f9fb9ce3993ed54d4e0a1ce
sohanemon <m.sohanemon@gmail.com>
2023-04-08 12:04:01 +0600

    implement ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main className=''></main>;
+  return <main className='text-9xl'></main>;
 }
