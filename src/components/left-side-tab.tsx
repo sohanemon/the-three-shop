6b7e9883136d7ce013a0eded438893b8a0199be9
sohanemon <m.sohanemon@gmail.com>
2023-04-08 22:32:06 +0600

    left bar customized

--- src/components/left-side-tab.tsx
+++ src/components/left-side-tab.tsx
@@ -7,6 +7,7 @@ export default function LeftSideTab() {
     <AnimatePresence>
       <motion.section
         initial={{ x: -100, opacity: 0 }}
+        className='absolute left-0 top-1/3 flex flex-col gap-3 -translate-y-1/2 bg-primary/10 px-2 py-4 rounded-xl'
         whileInView={{ x: 0, opacity: 1, transition: { delay: 1.1 } }}
       >
         {EditorTabs?.map((tab) => (
--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -1,11 +1,17 @@
-import { StaticImageData } from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 
 export default function Tab({
+  filter,
   name,
   icon,
 }: {
+  filter?: boolean;
   name: string;
   icon: StaticImageData;
 }) {
-  return <div>{name}</div>;
+  return (
+    <div>
+      <Image src={icon} alt='picker' width={30} />
+    </div>
+  );
 }
