5ca592dc0e4e6c42578e45c98bc2408c3358f0b9
sohanemon <m.sohanemon@gmail.com>
2023-04-08 12:18:36 +0600

    top -down animation added

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,11 +1,18 @@
 'use client';
+import { slideAnimation } from '@/lib/motion';
 import { AnimatePresence, motion } from 'framer-motion';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
     <AnimatePresence>
-      <motion.div>
-        <h1 className='text-3xl'>The Three Shop</h1>
+      <motion.div {...slideAnimation('down')}>
+        <div className='text-3xl flex items-center justify-center mt-3'>
+          <motion.div {...slideAnimation('left')}>
+            <Image src='/threejs.png' alt='Icon' width={50} height={50} />
+          </motion.div>
+          <motion.h1 {...slideAnimation('right')}>The Three Shop</motion.h1>
+        </div>
       </motion.div>
       ;
     </AnimatePresence>
