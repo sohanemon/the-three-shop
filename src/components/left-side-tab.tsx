aae726de03f2e7bd9eba4b34da4e330827b43376
sohanemon <m.sohanemon@gmail.com>
2023-04-09 10:09:56 +0600

    left side bug fixed

--- src/components/left-side-tab.tsx
+++ src/components/left-side-tab.tsx
@@ -4,16 +4,17 @@ import Tab from './tab';
 
 export default function LeftSideTab() {
   return (
-    <AnimatePresence>
+    <>
       <motion.section
         initial={{ x: -100, opacity: 0 }}
         className='absolute left-0 top-1/3 flex flex-col gap-3 -translate-y-1/2 bg-primary/10 px-2 py-4 rounded-xl'
         animate={{ x: 0, opacity: 1, transition: { delay: 0.4 } }}
+        exit={{ x: -100, opacity: 0, transition: { delay: 0 } }}
       >
         {EditorTabs?.map((tab) => (
           <Tab key={tab.name} {...tab} />
         ))}
       </motion.section>
-    </AnimatePresence>
+    </>
   );
 }
--- src/components/shirt.tsx
+++ src/components/shirt.tsx
@@ -1,7 +1,8 @@
+import { fadeAnimation } from '@/lib/motion';
 import { motion } from 'framer-motion';
 export default function Shirt() {
   return (
-    <motion.div>
+    <motion.div {...fadeAnimation}>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, sunt. Quae
       praesentium repellat ut possimus asperiores neque unde aspernatur deleniti
       iure modi aliquid eos, quia, aperiam, quos beatae reiciendis atque!
