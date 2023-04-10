4ba38ba09884470820f886fa140e9d3ef212781f
sohanemon <m.sohanemon@gmail.com>
2023-04-10 21:00:39 +0600

    performance issue with backdrop

--- src/features/canvas/backdrop.tsx
+++ src/features/canvas/backdrop.tsx
@@ -3,12 +3,17 @@ import { RandomizedLight, AccumulativeShadows } from '@react-three/drei';
 export default function Backdrop() {
   return (
     <AccumulativeShadows
-      position={[0, 0, -0.3]}
+      position={[0, 0, -0.1]}
       frames={60}
       alphaTest={0.85}
-      temporal
+      rotation={[Math.PI / 2, 0, 0]}
     >
-      <RandomizedLight amount={1} position={[1, 1, -1]} />
+      <RandomizedLight
+        intensity={1}
+        radius={9}
+        amount={4}
+        position={[5, 5, -10]}
+      />
     </AccumulativeShadows>
   );
 }
