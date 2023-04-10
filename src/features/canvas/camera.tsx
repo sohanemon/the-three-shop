a20d03e6ce07a47423a039a8a27cd5ef030aea8f
sohanemon <m.sohanemon@gmail.com>
2023-04-10 11:31:09 +0600

    easing damp added

--- src/features/canvas/camera.tsx
+++ src/features/canvas/camera.tsx
@@ -1,9 +1,24 @@
-import { PerspectiveCamera } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useRef } from 'react';
+import { Group } from 'three';
 
 export default function Camera({ children }: { children: React.ReactNode }) {
+  const groupRef = useRef<Group>(null);
+  useFrame((state, delta) => {
+    easing.damp3(state.camera.position, [0, 0, 5]);
+    easing.dampE(
+      groupRef?.current?.rotation!,
+      [state.pointer.x / 10, -state.pointer.y / 5, 0],
+      0.25,
+      delta
+    );
+  });
   return (
     <>
-      <perspectiveCamera position={[0, 0, 4.5]}>{children}</perspectiveCamera>
+      <perspectiveCamera position={[0, 0, 4.5]}>
+        <group ref={groupRef}>{children}</group>
+      </perspectiveCamera>
     </>
   );
 }
