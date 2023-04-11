615d8e88c3484c326e5a97f502ff29a5f2ad1d29
sohanemon <m.sohanemon@gmail.com>
2023-04-11 09:46:27 +0600

    stopPropagation added to the leftSideTab

--- src/components/left-side-tab.tsx
+++ src/components/left-side-tab.tsx
@@ -17,6 +17,7 @@ export default function LeftSideTab() {
   return (
     <>
       <motion.section
+        onClick={(e) => e.stopPropagation()}
         initial={{ x: -100, opacity: 0 }}
         className='absolute left-0 top-1/3 flex flex-col gap-3 -translate-y-1/2 bg-primary/10 px-2 py-4 rounded-xl'
         animate={{ x: 0, opacity: 1, transition: { delay: 0.4 } }}
